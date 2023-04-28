4a884a6fe8b708a7323f9de3b4dc131361121e0f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-28 12:32:36 +0300

    Added a docstring for the root endpoint

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+"""The entry point for the application."""
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -5,4 +6,9 @@ app = FastAPI()
 
 @app.get('/')
 async def root():
+    """The root endpoint for the application
+
+    Returns:
+        Dict: A dictionary containing a simple message for testing purposes
+    """
     return {'Working': 'Like a butterfly, crash like a bee'}
